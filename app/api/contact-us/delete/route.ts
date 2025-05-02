a1469568feed79b538a139c7363b4d3f314e7ad9
AnserShafiq <ansershafiq@gmail.com>
2025-05-02 21:56:32 +0500

    May 02,2025: Completed the messages listing, addition, deletion and detailed view.

--- app/api/contact-us/delete/route.ts
+++ app/api/contact-us/delete/route.ts
@@ -6,7 +6,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function DELETE(req:NextRequest){
     try {
         const id = await req.json();
-        await executeQuery('DELETE FROM contact_form WHERE id=?',[id[0]]);
+        if(Array.isArray(id[0]) ){
+            id[0].map(async(target:string) => 
+            {                
+                await executeQuery(`DELETE FROM contact_form WHERE id=?`,[target]);
+            } );
+
+
+        }else{
+            await executeQuery('DELETE FROM contact_form WHERE id=?',[id[0]]);
+        }
         return NextResponse.json({status:200})
     } catch (error) {
         console.error('Unable to delete message', error)
