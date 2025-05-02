9184f9d384710c3a15c9ed4dcc051decfdef1a9f
AnserShafiq <ansershafiq@gmail.com>
2025-05-02 18:51:17 +0500

    May 02,2025: Completing the contact form

--- app/ui/applicationForms/contactus-form.tsx
+++ app/ui/applicationForms/contactus-form.tsx
@@ -1,9 +1,13 @@
 import { InputField, Label } from "./formsBody/inputField";
 import SelectOptions from "./formsBody/select-options";
+import LeftHeading from "./left-heading";
 
 
 export default function ContactUsForm(){
     return <div className="w-full h-full bg-gray-100 flex flex-col justify-center p-10">
+        <div className="flex flex-col items-center w-full">
+            <LeftHeading>To Leave us a Message</LeftHeading>
+        </div>
         <h2 className="text-md lg:text-lg font-normal font-poppins ">Fill the form given below, we will reach back to you shortly.</h2>  
         <form className="flex flex-col gap-5 font-poppins mt-2">
             <div className="grid grid-cols-2 w-full gap-6 min-w-[600px]">
@@ -17,7 +21,7 @@ export default function ContactUsForm(){
                     <option value={'Other'}>Other</option>
                 </SelectOptions>
                 <div className="col-span-2">
-                    <Label name="message" label="Message" />
+                    <Label name="message" label="Message" required />
                     <textarea rows={3} name="message" id="message" className="w-full bg-gray-200 rounded-xl p-2" />
                 </div>
                 <div className="col-span-2 flex items-center justify-center">
